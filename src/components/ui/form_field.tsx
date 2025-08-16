03d59a99ebd9c5a5b9dcc91107b9e1e158e715ee
sael17 <sael8018@gmail.com>
2025-08-16 11:48:48 -0400

    Added name field to form field component and changed message field for a textarea HTML tag

--- src/components/ui/form_field.tsx
+++ src/components/ui/form_field.tsx
@@ -6,6 +6,7 @@ interface FormFieldProps extends React.ComponentProps<"div"> {
   inputClassName?: string;
   type?: React.InputHTMLAttributes<HTMLInputElement>["type"];
   value?: string;
+  name?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
@@ -15,18 +16,20 @@ const FormField: React.FC<FormFieldProps> = ({
   type,
   value,
   className,
+  name,
   onChange,
   ...props
 }) => {
   return (
     <div
       data-slot="form-field"
-      {...props}
+      {...props} 
     >
       <input 
       type={type} 
       placeholder={placeholder}
       value={value}
+      name={name}
       onChange={onChange}
       className={cn("p-2 border-3 border-solid border-black text-black rounded-md", inputClassName)}
       />
